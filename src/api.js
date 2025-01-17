c781f864a7669593e08ab1730b647641517cdad7
pratham182 <arorapratham900@gmail.com>
2025-01-17 22:39:05 +0530

    new-changes

--- src/api.js
+++ src/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:2000";
+
 export const submitUserData = async (submissionData) => {
   try {
-    const response = await axios.post("http://localhost:2000/api/submissions/", submissionData, {
+    const response = await axios.post(`${backendUrl}/api/submissions/`, submissionData, {
       headers: { "Content-Type": "multipart/form-data" },
     });
     return { success: true, message: response.data.message };
@@ -18,7 +21,7 @@ export const submitUserData = async (submissionData) => {
 
 export const fetchUsers = async () => {
   try {
-    const response = await axios.get("http://localhost:2000/api/users"); 
+    const response = await axios.get(`${backendUrl}/api/users`);
     return { success: true, data: response.data };
   } catch (error) {
     return {
